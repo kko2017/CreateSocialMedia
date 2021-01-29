b729fc7cd8c0e31ca0a78e104a326bf8a29bdd8b
KwangEun Oh <kwangeunoh1@gmail.com>
2021-01-29 03:46:46 -0400

    🛠 dynamic router for single post

---
+++ front/pages/post/_id/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container v-if="post">
+        <post-card :post="post" />
+    </v-container>
+    <div v-else>
+        That post doesn't exist.
+    </div>
+</template>
+<script>
+import PostCard from '~/components/PostCard'
+
+export default {
+    components: {
+        PostCard,
+    },
+    computed: {
+        post() {
+            return this.$store.state.posts.mainPosts.find(v => v.id === parseInt(this.$route.params.id, 10));
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
