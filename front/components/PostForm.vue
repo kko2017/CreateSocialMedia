de2f8f25499d6fe0fbc792449b63b0ab8de10a62
KwangEun Oh <kwangeunoh1@gmail.com>
2021-01-28 22:55:37 -0400

    🔧 test post form with dummy data

---
+++ front/components/PostForm.vue
@@ -0,0 +1,74 @@
+<template>
+    <v-card style="margin-bottom: 20px;">
+        <v-container>
+            <v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
+                <v-textarea
+                    v-model="content"
+                    outlined
+                    auto-grow
+                    clearable
+                    label="Share your awesome moments!!"
+                    :hide-details="hideDetails"
+                    :success-messages="successMessages"
+                    :success="success"
+                    :rules="[v => !!v.trim() || 'Input your story.']"
+                    @input="onChangeTextarea"
+                />
+                <v-btn type="submit" color="green" absolute right>ChitChat</v-btn>
+                <v-btn>Image Upload</v-btn>
+            </v-form>
+        </v-container>
+    </v-card>    
+
+</template>
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    data() {
+        return {
+            valid: false,
+            content: '',
+            hideDetails: false,
+            successMessages: '',
+            success: false
+        }
+    },
+    computed: {
+        ...mapState('users', ['me']),
+    },
+    methods: {
+        onChangeTextarea() {
+            this.hideDetails = true;
+            this.success = false;
+            this.successMessages = '';
+        },
+        onSubmitForm() {
+            if (this.$refs.form.validate()) {
+                this.$store.dispatch('posts/add', {
+                    content: this.content,
+                    User: {
+                        nickname: this.me.nickname
+                    },
+                    Comments: [],
+                    Images: [],
+                    id: Date.now(),
+                    createdAt: Date.now()
+                })
+                .then(() => {
+                    this.content = '';
+                    this.hideDetails = false;
+                    this.success = true;
+                    this.successMessages = 'Post Success.';
+                })
+                .catch(() => {
+
+                });
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
--- front/store/posts.js
+++ front/store/posts.js
@@ -1,9 +1,24 @@
 export const state = () => ({
-    name: 'posts',
+    mainPosts: [],
 });
 
 export const mutations = {
-    bye(state) {
-        state.name = 'good bye posts';
+    addMainPost(state, payload) {
+        // state.mainPosts.push(payload);
+        state.mainPosts.unshift(payload);
+    },
+    removeMainPost(state, payload) {
+        const index = state.mainPosts.findIndex(v => v.id === payload.id);
+        state.mainPosts.splice(index, 1);
+    }
+};
+
+export const actions = {
+    add({ commit }, payload) {
+        // 서버에 게시글 등록 요청 보냄
+        commit('addMainPost', payload);
+    },
+    remove({ commit }, payload) {
+        commit('removeMainPost', payload);
     }
 };
