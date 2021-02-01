b3c08d606cc48ca211b333c2705586290f9a03f8
KwangEun Oh <kwangeunoh1@gmail.com>
2021-02-01 14:24:44 -0400

    🛠 meta tags and favicon

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -1,16 +1,37 @@
 module.exports = {
     head: {
-        'title': 'ChitchatSM'
+        title: 'ChitchatSM',
+        meta: [{
+            charset: 'utf-8',
+        }, {
+            name: 'viewport',
+            content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover',
+        }, {
+            'http-equiv': 'X-UA-Compatible', content: 'IE=edge',
+        }, {
+            hid: 'description', name: 'description', content: 'ChitchatSM of FifthOh',
+        }, {
+            hid: 'ogtitle', name: 'og:title', content: 'ChichatSM',            
+        }, {
+            hid: 'ogdesc', name: 'og:description', content: 'ChitchatSM of FifthOh',
+        }, {
+            hid: 'ogtype', property: 'og:type', content: 'website',
+        }, {
+            hid: 'ogimg', property: 'og:image', content: '',
+        }, {
+            hid: 'ogurl', property: 'og:url', content: 'http://localhost:3080',
+        }],
+        link: [{ rel: 'shortcut icon', href: '/chitchat-sm.png' }],
     },
     modules: [
-        '@nuxtjs/axios'
+        '@nuxtjs/axios',
     ],
     buildModules: [
         '@nuxtjs/vuetify',
-        '@nuxtjs/moment'
+        '@nuxtjs/moment',
     ],
     moment: {
-        locales: ['ko']
+        locales: ['ko'],
     },
     build: {
         analyze: false,
@@ -18,8 +39,8 @@ module.exports = {
             if (isServer && !isDev) {
                 config.devtool = 'hidden-source-map';
             }
-            console.log('webpack', config, isServer, isClient);
-        }
+            // console.log('webpack', config, isServer, isClient);
+        },
     },
     plugins: [
 
@@ -30,9 +51,9 @@ module.exports = {
     axios: {
         browserBaseURL: 'http://localhost:3085',
         baseURL: 'http://localhost:3085',
-        https: false
+        https: false,
     },
     server: {
-        port: 3080
+        port: 3080,
     }
 };
--- front/pages/post/_id/index.vue
+++ front/pages/post/_id/index.vue
@@ -20,6 +20,22 @@ export default {
     },
     fetch({ store, params }) {
         return store.dispatch('posts/loadPost', params.id);
+    },
+    head() {
+        return {
+            title: `${this.post.User.nickname} Post`,
+            meta: [{
+                hid: 'description', name: 'description', content: this.post.content,
+            }, {
+                hid: 'ogtitle', name: 'og:title', content: `${this.post.User.nickname} post`,            
+            }, {
+                hid: 'ogdesc', name: 'og:description', content: this.post.content,
+            }, {
+                hid: 'ogimg', property: 'og:image', content: this.post.Images[0] ? this.post.Images[0].src : '',
+            }, {
+                hid: 'ogurl', property: 'og:url', content: `https://localhost:3080/post/${this.post.id}`,
+            }],
+        }
     }
 }
 </script>
